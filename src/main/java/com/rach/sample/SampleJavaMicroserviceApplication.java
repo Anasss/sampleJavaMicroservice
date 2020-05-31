828aa0137f6ee084e22e9143ab48f17d9acfbaba
arach <anass.rach@amadeus.com>
2020-05-31 09:25:13 +0200

    Initiate project setup!

---
+++ src/main/java/com/rach/sample/SampleJavaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rach.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rach/sample/SampleJavaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rach.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
