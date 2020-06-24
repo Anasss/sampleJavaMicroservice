defc46b2583a3bdf4f4c33a6ec62b4acf03734ed
arach <anass.rach@amadeus.com>
2020-06-24 22:26:13 +0200

    Adding Dockerfile and updating entities!

--- src/main/java/com/rach/sample/data.entity/Room.java
+++ src/main/java/com/rach/sample/data.entity/Room.java
@@ -1,4 +1,28 @@
 package src.main.java.com.rach.sample.data.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name="ROOM")
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Setter(value = AccessLevel.PUBLIC)
+@Getter
 public class Room {
-}
+  @Id
+  @Column(name="ROOM_ID")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long roomId;
+  @Column(name="NAME")
+  private String roomName;
+  @Column(name="ROOM_NUMBER")
+  private String roomNumber;
+  @Column(name="BED_INFO")
+  private String bedInfo;
+}  
--- src/main/java/com/rach/sample/repository/RoomRepository.java
+++ src/main/java/com/rach/sample/repository/RoomRepository.java
@@ -1,4 +1,9 @@
 package src.main.java.com.rach.sample.repository;
 
-public class RoomRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import src.main.java.com.rach.sample.data.entity.Room;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room, Long> {
 }
